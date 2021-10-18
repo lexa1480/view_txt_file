5faced16899873ee6376a33ab77acf34091325cf
Alex <lexa.f14@gmail.com>
2021-10-18 21:08:56 +0300

    Added --no_move and --no_resize

--- main.cpp
+++ main.cpp
@@ -3,41 +3,57 @@
 
 #include <QApplication>
 #include <QFile>
-#include <QByteArray>
 #include <QTextStream>
-
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
-{
+{ 
     boost::program_options::variables_map vm;
     if( CheckCommandLineArgs( argc, argv, vm ) )
     {
         if( vm.count(c_szArgPathFile) )
         {
-            QFile file( (vm[c_szArgPathFile].as<std::string>()).c_str() );
+            QTextCodec::setCodecForLocale(QTextCodec::codecForName("CP-1251"));
+            QTextCodec::setCodecForCStrings(QTextCodec::codecForName("CP-1251"));
+            QFile file( ((vm[c_szArgPathFile].as<std::string>()).c_str()) );
             if (file.open(QIODevice::ReadOnly | QIODevice::Text))
             {
+                QApplication a(argc, argv);
+                MainWindow w;
+
                 QTextStream qtsInFile(&file);
                 if(vm.count(c_szArgTypeCoding))
                 {
                     qtsInFile.setCodec((vm[c_szArgTypeCoding].as<std::string>()).c_str());
                 }
                 QString qsResText = qtsInFile.readAll();
+                w.SetText( qsResText );
 
-                QApplication a(argc, argv);
-                MainWindow w;
+                QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+                QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
                 if(vm.count(c_szArgTitle))
                 {
                     w.setWindowTitle((vm[c_szArgTitle].as<std::string>()).c_str());
                 }
                 else
                 {
-                    std::string sTitle = file.fileName().toStdString();
+                    std::string sTitle = vm[c_szArgPathFile].as<std::string>();
                     sTitle = sTitle.substr(sTitle.find_last_of("\\/") + 1,std::string::npos);
-                    w.setWindowTitle(sTitle.c_str());
+                    w.setWindowTitle( sTitle.c_str() );
                 }
-                w.SetText( qsResText );
+
+                if(vm.count(c_szArgNoMove))
+                {
+                    w.setWindowFlags(Qt::CustomizeWindowHint);
+                }
+
                 w.show();
+                if(vm.count(c_szArgNoResize))
+                {
+                    w.setFixedSize(w.size());
+                }
+
+
                 return a.exec();
             }
             else
---
+++ tmp/view_txt_file.bin/debug/ui/ui_mainwindow.h
@@ -0,0 +1,84 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 4.8.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QGridLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QMainWindow>
+#include <QtGui/QTextBrowser>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QGridLayout *gridLayout_2;
+    QTextBrowser *textBrowser;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->setWindowModality(Qt::WindowModal);
+        MainWindow->setEnabled(true);
+        MainWindow->resize(1012, 605);
+        QSizePolicy sizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMouseTracking(false);
+        MainWindow->setTabShape(QTabWidget::Rounded);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        centralwidget->setEnabled(true);
+        centralwidget->setMinimumSize(QSize(0, 0));
+        centralwidget->setMaximumSize(QSize(10000, 10000));
+        gridLayout_2 = new QGridLayout(centralwidget);
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        textBrowser = new QTextBrowser(centralwidget);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+        textBrowser->setEnabled(true);
+        QSizePolicy sizePolicy1(QSizePolicy::Maximum, QSizePolicy::Maximum);
+        sizePolicy1.setHorizontalStretch(100);
+        sizePolicy1.setVerticalStretch(100);
+        sizePolicy1.setHeightForWidth(textBrowser->sizePolicy().hasHeightForWidth());
+        textBrowser->setSizePolicy(sizePolicy1);
+        textBrowser->setMaximumSize(QSize(10000, 10000));
+
+        gridLayout_2->addWidget(textBrowser, 0, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralwidget);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
